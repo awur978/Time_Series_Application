2a301cd86243d4aa599aaae140244182bd395037
Adedamola Wuraola <32375311+awur978@users.noreply.github.com>
2018-11-11 11:12:08 +1300

    Update apply.m

--- Activation_Functions/+sqlu/apply.m
+++ Activation_Functions/+sqlu/apply.m
@@ -2,5 +2,4 @@ function a = apply(n,param)
 
 % Copyright 2017 The MathWorks, Inc.
 
-alpha1 = 1;
-a = n.*(n>0) + alpha1.*((n + n.^2/4).*(-2.0 <= n & n <= 0) + (-1) .*(n<-2.0));
+#will include it when it is published
