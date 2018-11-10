60818891e62c1bc64225d2ff270edce459aa027c
Adedamola Wuraola <32375311+awur978@users.noreply.github.com>
2018-11-11 11:12:50 +1300

    Update backprop.m

--- Activation_Functions/+sqlu/backprop.m
+++ Activation_Functions/+sqlu/backprop.m
@@ -2,5 +2,5 @@ function dn = backprop(da,n,a,param)
 %SQLU.BACKPROP
 alpha1 = 1.0;
 % Copyright 2017 The MathWorks, Inc.
-dn = bsxfun(@times,da, 1.*(n>0) + alpha1.*((1 + n/2).*(-2.0 <= n & n <= 0) + (0) .*(n<-2.0)));
+#will include it when it is published
 end
