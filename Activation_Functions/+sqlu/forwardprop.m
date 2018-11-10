25986e03d196763cf96aae445f682a53b30b0d24
Adedamola Wuraola <32375311+awur978@users.noreply.github.com>
2018-11-11 11:13:11 +1300

    Update forwardprop.m

--- Activation_Functions/+sqlu/forwardprop.m
+++ Activation_Functions/+sqlu/forwardprop.m
@@ -3,7 +3,5 @@ function da = forwardprop(dn,n,a,param)
 
 % Dept of ECE, UoA (dn,n,a,param)
 
-alpha1 = 1;
-d =  1.*(n>0) + alpha1.*((1 + n/2).*(-2.0 <= n & n <= 0) + (0) .*(n<-2.0));
-da = bsxfun(@times,dn,d);
+#will include it when it is published
 end
