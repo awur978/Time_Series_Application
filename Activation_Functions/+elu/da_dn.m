481504ec7f3ea2481bbf132aea463064e9e1f8df
Adedamola Wuraola <32375311+awur978@users.noreply.github.com>
2020-01-24 09:27:45 +1300

    Update da_dn.m

--- Activation_Functions/+elu/da_dn.m
+++ Activation_Functions/+elu/da_dn.m
@@ -1,12 +1,5 @@
 function d = da_dn(n,a,param)
 
-% Copyright 2012 The MathWorks, Inc.
 alpha1 = 1;
-
-  %d = max(n>1,a+alpha1);
-
-  d =  1.*(n > 0) + (alpha1+a).*(n <= 0);
-  
-  
-  %dLdX = dLdZ .* ((X > 0) + ...
-                %((layer.alpha + Z) .* (X <= 0))); 
+d =  1.*(n > 0) + (alpha1+a).*(n <= 0);
+end
